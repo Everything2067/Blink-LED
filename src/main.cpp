9661ab7aeb82d270a3d0c6c4d3ae2e645e9ea523
Everything2067 <anandshaurya2009@gmail.com>
2025-02-17 20:46:38 +0530

    Removed unnecessary flags and added universal ones

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,26 @@
 /*
-  Blink by Everything2067.
+  BlinkLED by Everything2067.
   Blinks an LED connected to the defined pin.
   By default, it will blink an LED connected to the built-in LED, however you can change this, and the on and off time as well.
 */
 
-#include <Arduino.h>
-
-// #define LED
+// Uncomment this if you want to define a specific pin.
+// #define LED 13
 
 #ifndef LED
-  #ifdef UNO_R3
+  #ifdef ARDUINO
     #define LED 13
   #endif
-
   #ifdef NODEMCU_32S
     #define LED 2
   #endif
 #endif
 
-#define ON_TIME 1000
+// Including the required library.
+#include <Arduino.h>
+
+// Defining delays. You can change this as per your need.
+#define ON_TIME  1000
 #define OFF_TIME 1000
 
 void setup() {
@@ -28,6 +30,7 @@ void setup() {
 void loop() {
   digitalWrite(LED, HIGH);
   delay(ON_TIME);
+
   digitalWrite(LED, LOW);
   delay(OFF_TIME);
 }
